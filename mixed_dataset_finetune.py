81e9afab70267c665313f2ab2357f8798fb598f0
adriannunezmarcos <adriannunezmarcos@gmail.com>
2023-07-10 15:43:42 +0200

    Configuration files added

--- mixed_dataset_finetune.py
+++ mixed_dataset_finetune.py
@@ -1,5 +1,4 @@
 import os
-#os.environ['CUDA_VISIBLE_DEVICES']=''
 os.environ['CUDA_LAUNCH_BLOCKING']='1'
 
 import yaml
@@ -7,7 +6,7 @@ import torch
 import random
 import numpy as np
 from model import VideoTransformer
-from dataset import FallDataset, load_labels
+from dataset import FallDataset, URFallDataset, UPURFallDataset, load_labels
 from kinetics_class_index import kinetics_classnames
 
 def show_msg(msg):
@@ -27,24 +26,29 @@ with open("config_eval2.yaml", "r") as stream:
     config = yaml.safe_load(stream)
 
 train_labels, dev_labels, test_labels = load_labels(config)
-train_dataset = FallDataset(config, train_labels, 'train')
-dev_dataset = FallDataset(config, dev_labels, 'dev')
+train_dataset = UPURFallDataset(config, train_labels, 'train')
+dev_dataset = UPURFallDataset(config, dev_labels, 'dev')
 print('Train samples: {}'.format(len(train_dataset.labels)))
 print('Dev samples: {}'.format(len(dev_dataset.labels)))
 
 model = VideoTransformer(config)
-#model.load_state_dict(torch.load(config["model_save_path"]))
 model.train(config, train_dataset, dev_dataset)
 del train_dataset, dev_dataset, train_labels, dev_labels
 torch.save(model.state_dict(), config["model_save_path"])
 
 test_dataset = FallDataset(config, test_labels, 'test')
+ur_fall_test_dataset = URFallDataset(config, 'test')
 
 print('Test samples UP-Fall: {}'.format(len(test_dataset)))
+print('Test samples UR-Fall: {}'.format(len(ur_fall_test_dataset)))
 
 if config["binarise"]:
     show_msg('UP Fall test set results')
     model.test(config, test_dataset)
+    show_msg('UR Fall test set results')
+    model.test(config, ur_fall_test_dataset)
 
 show_msg('UP Fall test set results by clip')
 model.test_by_clip(config, test_dataset)
+show_msg('UR Fall test set results by clip')
+model.test_by_clip(config, ur_fall_test_dataset)
